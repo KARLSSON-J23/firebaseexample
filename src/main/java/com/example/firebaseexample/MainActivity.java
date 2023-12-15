8379d5e048886e74a928d1f418d9b954120fcdf0
KARLSSON-J23 <hcps98023@gmail.com>
2023-12-15 23:47:54 +0800

    java update

--- src/main/java/com/example/firebaseexample/MainActivity.java
+++ src/main/java/com/example/firebaseexample/MainActivity.java
@@ -1,56 +1,57 @@
-package com.example.firebaseexample;
+    package com.example.firebaseexample;
 
-import static android.content.ContentValues.TAG;
+    import static android.content.ContentValues.TAG;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+    import androidx.annotation.NonNull;
+    import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
+    import android.os.Bundle;
+    import android.util.Log;
+    import android.widget.TextView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.IgnoreExtraProperties;
-import com.google.firebase.database.ValueEventListener;
+    import com.google.firebase.database.DataSnapshot;
+    import com.google.firebase.database.DatabaseError;
+    import com.google.firebase.database.DatabaseReference;
+    import com.google.firebase.database.FirebaseDatabase;
+    import com.google.firebase.database.IgnoreExtraProperties;
+    import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
+    import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity {
-    private DatabaseReference mDatabase;
-    private TextView show;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public class MainActivity extends AppCompatActivity {
+        private DatabaseReference mDatabase;
+        private TextView show;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
+            show = findViewById(R.id.textShow);
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference("message");
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("key");
+            //寫入值
+            myRef.setValue("你好");
 
-        //myRef.setValue("Hello, World!");
+            // Read from the database
+            myRef.addValueEventListener(new ValueEventListener() {
+                //讀取資料
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // 使用初始值呼叫此方法一次，然後再次調用
+                    // 每當此位置的資料更新時。
+                    String value = dataSnapshot.getValue(String.class);
+                    show.setText(value);
+                    Log.d("TAG", "Value is: " + value);
+                }
 
-        show = findViewById(R.id.textShow);
-// Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue().toString();
-                show.setText("AAA" + value);
-                Log.d(TAG, "Value is: " + value);
-            }
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // 讀取值失敗
+                    Log.w("TAG", "Failed to read value.", error.toException());
+                }
+            });
 
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
+        }
 
     }
-
-}
